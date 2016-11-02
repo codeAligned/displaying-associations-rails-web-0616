d80092e0dae335bac9c5c9f4c622412aa26594cf
gyeon <gyeon19@gmail.com>
2016-11-02 18:00:40 -0400

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,17 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(name: params[:name])
+  end
+
+  def index
+    @categories = Category.all
+  end
+
 end
